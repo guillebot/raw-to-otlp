ed060f96413cbe25ecd20b9a52c1ed27a67cdc5c
Guillermo Schimmel <gschimmel@gmail.com>
2025-08-19 11:50:11 -0400

    a

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,5 @@
-docker build -t gschimmel/transformers-sevone-to-otlp:1.2 .
-docker push gschimmel/transformers-sevone-to-otlp:1.2
+docker build -t gschimmel/transformers-sevone-to-otlp:1.3 .
+docker push gschimmel/transformers-sevone-to-otlp:1.3
+docker tag gschimmel/transformers-sevone-to-otlp:1.3 gschimmel/transformers-sevone-to-otlp:latest
 docker push gschimmel/transformers-sevone-to-otlp:latest
 
